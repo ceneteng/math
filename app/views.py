52e917cf590fd4d0d517a456dd7bbfb4ec6c957e
localadmin <eaton.chrisc@gmail.com>
2018-09-10 15:20:58 -0500

    answer

--- app/views.py
+++ app/views.py
@@ -60,10 +60,10 @@ def game():
     checknum = int(request.cookies.get('counter'))
     problems = int(request.cookies.get('problems'))
     ceiling = int(request.cookies.get('ceiling'))
-    prevans = int(request.cookies.get('answer'))
+    answer = int(request.cookies.get('answer'))
     if checknum < problems:	
         nums = getnums(ceiling)
-        return render_template('game.html',num0=nums[0],num1=nums[1],num2=nums[2],checknum=checknum,problems = problems, answer=prevans, form=form)
+        return render_template('game.html',num0=nums[0],num1=nums[1],num2=nums[2],checknum=checknum,problems = problems, answer=answer, form=form)
 
     else:
         return redirect(url_for('confgame'))
