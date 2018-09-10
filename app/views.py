43fd3b7ea6727206d3ca359a4f3df421f0b050b0
localadmin <eaton.chrisc@gmail.com>
2018-09-10 15:04:40 -0500

    forms

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@
 
 from flask import render_template, flash, redirect, request, url_for
 from app import app
-from .forms import nameform, gameform
+from .forms import nameform, gameform, playform
 from .mymath import getnums
 
 
